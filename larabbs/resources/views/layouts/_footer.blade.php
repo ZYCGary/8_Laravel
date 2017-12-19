e9b81f38a29bcd0e8ae44d6b8a78b6d72faada09
ZYCGary <zyc.gary@gmail.com>
2017-12-19 21:01:21 +1100

    Finish basic page layout

---
+++ larabbs/resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Designed and coded by <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Gary</a><span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">Contact Us</a></p>
+    </div>
+</footer>
---
+++ larabbs/resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home')
+
+@section('content')
+    <h1>This is Home page</h1>
+@stop
