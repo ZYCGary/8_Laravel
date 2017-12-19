9547983257532acf9228a2964b86d8b8741498f8
ZYCGary <zyc.gary@gmail.com>
2017-12-20 00:23:18 +1100

    Add captcha

--- larabbs/app/Http/Controllers/Auth/RegisterController.php
+++ larabbs/app/Http/Controllers/Auth/RegisterController.php
@@ -42,7 +42,7 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -51,13 +51,17 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'captcha' => 'required|captcha',
+        ], [
+            'captcha.required' => 'Verify code must be filled.',
+            'captcha.captcha' => 'Please enter correct verify code.',
         ]);
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \App\User
      */
     protected function create(array $data)
@@ -68,4 +72,6 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+
 }
