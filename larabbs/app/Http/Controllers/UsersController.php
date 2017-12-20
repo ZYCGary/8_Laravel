634512be315de15837b3da32d3d50fc3e7c05144
ZYCGary <zyc.gary@gmail.com>
2017-12-20 17:50:13 +1100

    User can upload avatar

--- larabbs/app/Http/Controllers/UsersController.php
+++ larabbs/app/Http/Controllers/UsersController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    public function show(User $user){
+    public function show(User $user)
+    {
         return view('users.show', compact('user'));
     }
 
@@ -17,9 +19,23 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
-        return redirect()->route('users.show', $user->id)->with('success', 'Profile is updated successfully!');
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                if ($user['avatar']) {
+                    $old_path = "../public" . str_replace(config('app.url'), '', $user['avatar']);
+                    unlink($old_path);
+                }
+
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', 'Profile\'ve been updated successfully！');
     }
 }
--- larabbs/app/Http/Requests/UserRequest.php
+++ larabbs/app/Http/Requests/UserRequest.php
@@ -18,6 +18,18 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'avatar.dimensions' => 'Image resolution is too low, wide and high need to be more than 200px.',
+            'name.unique' => 'The name has already been taken.',
+            'name.regex' => 'The username format is invalid. Only Chinese or English letters, numbers, ‘-’ and ‘_’ are supported.',
+            'name.between' => 'The username must be between 3 and 25 characters.',
+            'name.required' => 'The username field is required.',
         ];
     }
 }
