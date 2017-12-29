566bd311377f149462a7d61540cb338f086ffaee
ZYCGary <zyc.gary@gmail.com>
2017-12-29 12:48:27 +1100

    5.1 新建话题

--- larabbs/app/Models/Category.php
+++ larabbs/app/Models/Category.php
@@ -4,6 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Category
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name Category name
+ * @property string|null $description Category description
+ * @property int $post_count Number of post
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category wherePostCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereUpdatedAt($value)
+ */
 class Category extends Model
 {
     protected $fillable = [
