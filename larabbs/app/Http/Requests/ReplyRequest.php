183b08b9d0f9ef0ed37917529b5633764525d8ad
ZYCGary <zyc.gary@gmail.com>
2018-01-10 16:28:58 +1100

    6.2 发表回复

--- larabbs/app/Http/Requests/ReplyRequest.php
+++ larabbs/app/Http/Requests/ReplyRequest.php
@@ -2,34 +2,16 @@
 
 namespace App\Http\Requests;
 
+/**
+ * @property resource|string content
+ */
 class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
