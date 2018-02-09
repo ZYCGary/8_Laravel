ff0b4ac0f051ad8e459fa24ef505f39e9eb93435
ZYCGary <zyc.gary@gmail.com>
2018-02-09 16:05:24 +1100

    7.3 管理后台 - 初始化

--- larabbs/resources/views/layouts/_header.blade.php
+++ larabbs/resources/views/layouts/_header.blade.php
@@ -64,8 +64,18 @@
 
                         <ul class="dropdown-menu" role="menu">
 
+                            @can('manage_contents')
+                                <li>
+                                    <a href="{{ url(config('administrator.uri')) }}">
+                                        <span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span>
+                                        管理后台
+                                    </a>
+                                </li>
+                            @endcan
+
                             <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                                     Profile
                                 </a>
                             </li>
