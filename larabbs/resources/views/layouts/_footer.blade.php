c23becc113c7076342e8464075dc19b983b0ef70
ZYCGary <zyc.gary@gmail.com>
2019-05-10 09:59:46 +1000

    1

--- larabbs/resources/views/layouts/_footer.blade.php
+++ larabbs/resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            Designed and coded by <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Gary</a><span style="color: #e27575;font-size: 14px;">❤</span>
+            Designed and coded by <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Gary~~</a><span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="pull-right"><a href="mailto:name@email.com">Contact Us</a></p>
